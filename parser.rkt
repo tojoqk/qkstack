cfaf3021465d342ea42d734b949d00bc71a499a7
Masaya Tojo <masaya@tojo.tokyo>
2019-04-02 06:57:32 +0900

    add parser.rkt

---
+++ parser.rkt
@@ -0,0 +1,9 @@
+#lang brag
+
+%%qkstack      : %%block*
+%%block        : %%if-block | %%define-block | %%operator | %%datum
+%%if-block     : IF-BEGIN %%block* (END | IF-ELSE %%block* END)
+%%define-block : DEFINE-BEGIN %%block* END
+%%operator     : IDENTIFIER
+%%datum        : STRING | NUMBER | QUOTE %%sexp
+%%sexp         : QUOTE %%sexp | STRING | NUMBER | IDENTIFIER | LEFT-PARENCE %%sexp RIGHT-PARENCE
