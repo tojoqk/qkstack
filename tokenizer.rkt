d4b28292db2b13ab47ce9ad051fe13917b7adddb
Masaya Tojo <masaya@tojo.tokyo>
2019-04-02 07:10:31 +0900

    Modify tokenizer

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -7,10 +7,11 @@
   (define qkstack-lexer
     (lexer-src-pos
      [whitespace (token lexeme #:skip? #t)]
-     ["if[" (token 'IF-BEGIN lexeme)]
-     ["]else[" (token 'IF-ELSE lexeme)]
-     ["]" (token 'END 'end)]
-     [":[" (token 'DEFINE-BEGIN lexeme)]
+     ["if{" (token 'IF-BEGIN lexeme)]
+     ["}else{" (token 'IF-ELSE lexeme)]
+     ["{" (token 'BEGIN lexeme)]
+     ["}" (token 'END lexeme)]
+     [":" (token 'DEFINE-BEGIN lexeme)]
      [(:+ (char-set "0123456789"))
       (token 'NUMBER (string->number lexeme))]
      [(:+ (:~ (char-set " \n@()[]{}\",'`;#|\\")))
@@ -21,3 +22,4 @@
       (token 'STRING (trim-ends "\"" lexeme "\""))]
      [(eof) (void)]))
   (thunk (qkstack-lexer in)))
+(provide make-tokenizer)
