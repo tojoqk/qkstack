18d3afdfd96e168759ebd3b173be87920a632d51
Masaya Tojo <masaya@tojo.tokyo>
2019-04-02 07:10:46 +0900

    Modify parser

--- parser.rkt
+++ parser.rkt
@@ -1,9 +1,9 @@
 #lang brag
 
 %%qkstack      : %%block*
-%%block        : %%if-block | %%define-block | %%operator | %%datum
+%%block        : %%if-block | %%define-block | (%%operator | %%datum)+
 %%if-block     : IF-BEGIN %%block* (END | IF-ELSE %%block* END)
-%%define-block : DEFINE-BEGIN %%block* END
+%%define-block : DEFINE-BEGIN IDENTIFIER BEGIN %%block* END
 %%operator     : IDENTIFIER
 %%datum        : STRING | NUMBER | QUOTE %%sexp
 %%sexp         : QUOTE %%sexp | STRING | NUMBER | IDENTIFIER | LEFT-PARENCE %%sexp RIGHT-PARENCE
