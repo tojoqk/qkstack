e7f2d08871d7b91e7b5ce6b080105ffc6400eac4
Masaya Tojo <masaya@tojo.tokyo>
2019-04-02 06:57:26 +0900

    add tokenizer.rkt

---
+++ tokenizer.rkt
@@ -0,0 +1,23 @@
+#lang racket
+(require brag/support)
+
+(define (make-tokenizer in path)
+  (port-count-lines! in)
+  (lexer-file-path path)
+  (define qkstack-lexer
+    (lexer-src-pos
+     [whitespace (token lexeme #:skip? #t)]
+     ["if[" (token 'IF-BEGIN lexeme)]
+     ["]else[" (token 'IF-ELSE lexeme)]
+     ["]" (token 'END 'end)]
+     [":[" (token 'DEFINE-BEGIN lexeme)]
+     [(:+ (char-set "0123456789"))
+      (token 'NUMBER (string->number lexeme))]
+     [(:+ (:~ (char-set " \n@()[]{}\",'`;#|\\")))
+      (token 'IDENTIFIER (string->symbol lexeme))]
+     ["(" (token 'LEFT-PARENCE lexeme)]
+     [")" (token 'RIGHT-PARENCE lexeme)]
+     [(from/to "\"" "\"")
+      (token 'STRING (trim-ends "\"" lexeme "\""))]
+     [(eof) (void)]))
+  (thunk (qkstack-lexer in)))
