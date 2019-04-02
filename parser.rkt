a1d295f29d989904905ad41ec9d794bf12aa5141
Masaya Tojo <masaya@tojo.tokyo>
2019-04-03 05:05:51 +0900

    Fix parser

--- parser.rkt
+++ parser.rkt
@@ -1,9 +1,13 @@
 #lang brag
 
-%%qkstack      : %%block*
-%%block        : %%if-block | %%define-block | (%%operator | %%datum)+
-%%if-block     : IF-BEGIN %%block* (END | IF-ELSE %%block* END)
-%%define-block : DEFINE-BEGIN IDENTIFIER BEGIN %%block* END
-%%operator     : IDENTIFIER
-%%datum        : STRING | NUMBER | QUOTE %%sexp
-%%sexp         : QUOTE %%sexp | STRING | NUMBER | IDENTIFIER | LEFT-PARENCE %%sexp RIGHT-PARENCE
+%%qkstack    : %%expression*
+%%expression : (%%form | %%word | %%datum | %%block)
+%%form       : "(" (%%if | %%define | %%require | %%provide) ")"
+%%if         : "if" %%block %%block?
+%%define     : "define" IDENTIFIER %%block
+%%require    : "require" %%sexp+
+%%provide    : "provide" %%sexp+
+%%block      : "[" %%expression* "]"
+%%word       : IDENTIFIER
+%%datum      : STRING | NUMBER | TRUE | FALSE | "'" %%sexp
+%%sexp       : "'" %%sexp | STRING | NUMBER | IDENTIFIER | "(" %%sexp* ")"
