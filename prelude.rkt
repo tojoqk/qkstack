b31edb33b08363abc7337759007a82574b064854
Masaya Tojo <masaya@tojo.tokyo>
2019-04-13 12:05:12 +0900

    Fix prelude.rkt

--- prelude.rkt
+++ prelude.rkt
@@ -1,6 +1,6 @@
-#lang racket
-(require "stack.rkt")
-(require "util.rkt")
+#lang racket/base
+(require "stack.rkt"
+         "util.rkt")
 
 ;; stack processing
 (define/qkstack (dup a -> 2) (values a a))
@@ -24,18 +24,10 @@
 (define/qkstack (nip a b -> 1) b)
 (provide nip)
 
-(define/qkstack (tuck a b -> 3) (values b a b))
-(provide tuck)
-
 (define (%dump stack)
   (push! stack (dump stack))
   stack)
 (provide (rename-out [%dump dump]))
-
-(define (load stack)
-  (load! stack (pop! stack))
-  stack)
-(provide load)
 
 (define (clear stack)
   (clear! stack)
@@ -43,41 +35,19 @@
 (provide clear)
 
 (define (empty? stack)
-  (push! stack (stack-empty? stack))
+  (push! stack (empty-stack? stack))
   stack)
 (provide empty?)
 
-;; list processing
-(provide/qkstack [cons 2 -> 1]
-                 [car 1 -> 1]
-                 [cdr 1 -> 1]
-                 [null? 1 -> 1]
-                 [pair? 1 -> 1])
-
-;; arithmetic
-(provide/qkstack [zero? 1 -> 1]
-                 [sub1 1 -> 1]
-                 [add1 1 -> 1]
-                 [+ 2 -> 1]
-                 [- 2 -> 1]
-                 [* 2 -> 1]
-                 [/ 2 -> 1]
+(provide/qkstack [zero? 1 -> 1] [sub1 1 -> 1] [add1 1 -> 1]
+                 [+ 2 -> 1] [- 2 -> 1] [* 2 -> 1] [/ 2 -> 1]
                  [modulo 2 -> 1]
-                 [quotient 2 -> 1]
                  [= 2 -> 1]
-                 [< 2 -> 1]
-                 [> 2 -> 1]
-                 [<= 2 -> 1]
-                 [>= 2 -> 1])
-
-;; I/O
-(provide/qkstack [read-line 0 -> 1]
-                 [display 1 -> 0]
+                 [< 2 -> 1] [> 2 -> 1]
+                 [<= 2 -> 1] [>= 2 -> 1]
                  [displayln 1 -> 0]
                  [write 1 -> 0]
-                 [not 1 -> 1])
-
-;; equality
-(provide/qkstack [eq? 2 -> 1]
+                 [not 1 -> 1]
+                 [eq? 2 -> 1]
                  [eqv? 2 -> 1]
                  [equal? 2 -> 1])
