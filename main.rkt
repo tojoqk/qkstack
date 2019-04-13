1a8b7ae02860409f08923c18360cd04863fd089f
Masaya Tojo <masaya@tojo.tokyo>
2019-04-13 11:46:15 +0900

    Add parser's test

--- main.rkt
+++ main.rkt
@@ -1,7 +1,10 @@
 #lang racket/base
 (require syntax/strip-context
+         brag/support
          "parser.rkt" "tokenizer.rkt")
 
+(module+ test
+  (require rackunit))
 
 (define (read-syntax path port)
   (define stx
@@ -13,3 +16,26 @@
 
 (module+ reader
   (provide read-syntax))
+
+(module+ test
+  (define first-sample-code
+    "(require qkstack/prelude)
+     18 20 30 10 + 3 12 * - + +
+     displayln")
+  (check-equal?
+   (parse-to-datum
+    (make-tokenizer (open-input-string first-sample-code)))
+   '(%qkstack
+     (%form (%require "(" "require" qkstack/prelude ")"))
+     (%operator (%datum 18))
+     (%operator (%datum 20))
+     (%operator (%datum 30))
+     (%operator (%datum 10))
+     (%operator (%word +))
+     (%operator (%datum 3))
+     (%operator (%datum 12))
+     (%operator (%word *))
+     (%operator (%word -))
+     (%operator (%word +))
+     (%operator (%word +))
+     (%operator (%word displayln)))))
